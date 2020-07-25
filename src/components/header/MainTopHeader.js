c210fc98d0628de9da0f2bac3dce0f8b3be0812f
thisis-Solomon <solomonnjobvu1@gmail.com>
2020-07-25 16:11:38 +0200

    [added searchBar]

--- src/components/header/MainTopHeader.js
+++ src/components/header/MainTopHeader.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import {Appbar} from 'react-native-paper';
-import {View} from 'react-native'
+import React, {useState} from 'react';
+import {Appbar, Searchbar} from 'react-native-paper';
+import {View} from 'react-native';
 
 export default function MainTopHeader({openDialog}) {
   const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
 
+  const [isSearchBarVisible, setIsSearchBarVisible] = useState(false);
+
+  const SearchBar = !isSearchBarVisible ? (
+    <Appbar.Action
+      icon="magnify"
+      onPress={() => setIsSearchBarVisible(!isSearchBarVisible)}
+    />
+  ) : (
+    <Searchbar
+      style={{height: 38, alignSelf: 'center', borderRadius: 20}}
+      placeholder="Eg. 384"
+      maxLength = {3}
+      keyboardType = 'number-pad'
+      // onChangeText={onChangeSearch}
+      // value={searchQuery}
+    />
+  );
+
   return (
-    <View style={{flexDirection: 'row',}}>
-      <Appbar.Action icon="magnify" onPress={() => {}} />
+    <View style={{flexDirection: 'row'}}>
+      {SearchBar}
       <Appbar.Action icon={MORE_ICON} onPress={openDialog} />
     </View>
   );
